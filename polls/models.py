59d81d186a0c2b9c9899a95ae327d686b2a41bee
cleyber <cleyber65@gmail.com>
2017-04-17 16:58:04 -0500

    Testing, parte 5

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,24 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible # solo si se necesita soportar python 2
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
 
-class Choise(models.Model):
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+@python_2_unicode_compatible
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choise_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
